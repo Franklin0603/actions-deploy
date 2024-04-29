325018ba6524b6e64b8c1f306c2b9092676db170
Franklin Ajisogun <franklin.ajisogun03@gmail.com>
2024-04-28 20:33:51 -0400

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,12 @@
-from app import app
+import sys
+import os
+
+# Append the directory above 'tests' to sys.path
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# parent_dir = os.path.dirname(current_dir)
+# sys.path.append(parent_dir)
+
+from app.src.app import app
 
 def test_home():
     client = app.test_client()
